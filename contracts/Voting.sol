29070bc3501de7b2d929ac29058579a770cf51e5
Manoj Azad <manojazad@Manojs-MacBook-Pro.local>
2023-03-03 13:14:58 +0530

    Updated contract

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,32 +1,25 @@
-//SPDX-License-Identifier:UNLICENSED;
+//SPDX-License-Identifier:UNLICENSED
 
 pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-Contract Voting {
-    
+contract Voting {
     address[] public listOfVoters;
     mapping(address => bool) userAlreadyVoted;
     IERC721 public erc721;
-    
-    constructor (address _nftAddress) {
-      erc721 = IERC721(_nftAddress);
-    }
-    
 
-    function vote() external returns(string memory) {
-
-        if(!userAlreadyVoted[msg.sender]) {
-       require(erc721.balance(msg.sender) > 0, "You Don't have NFT");
-       listOfVoters.push(msg.sender);
-       userAlreadyVoted[msg.sender] = true;
-    }
-    else {
-        revert("Already voted");
+    constructor(address _nftAddress) {
+        erc721 = IERC721(_nftAddress);
     }
 
-}
-
-
+    function vote() external {
+        if (!userAlreadyVoted[msg.sender]) {
+            require(erc721.balanceOf(msg.sender) > 0, "You Don't have NFT");
+            listOfVoters.push(msg.sender);
+            userAlreadyVoted[msg.sender] = true;
+        } else {
+            revert("Already voted");
+        }
+    }
 }
